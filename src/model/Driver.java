f3802c5307dee26bf850e7a9b8db010f85127ac4
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2023-06-29 03:36:12 -0400

    initial commit

--- src/model/Driver.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public class Driver {
-    public static void main(String[] args) {
-        Customer customer = new Customer("Mohamed", "Doe", "mohamedDoe26@gmail.com");
-        System.out.println(customer);
-    }
-}
